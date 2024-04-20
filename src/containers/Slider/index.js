d0d5dd3b46da7ca8ca0fff1b63a842d7fd125172
Rougui224 <rouguiatoubarry559@gmail.com>
2024-04-20 11:56:42 +0200

    first commit

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+
+  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  ); // ajout de -1 à lenght pour eviter que l'index depasse 2
+
+  const nextCard = () => {
+    setTimeout(
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      5000
+    );
+    // console.log(`voici l'index avec use effect`, index);
+  };
+  useEffect(() => {
+    nextCard();
+  });
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <div key={`${event.title}`}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((e, radioIdx) => (
+                <input
+                  key={e.title}
+                  type="radio"
+                  name="radio-button"
+                  // modification de l'index pour suivre le slider
+                  checked={index === radioIdx}
+                  onChange={() => {
+                    setIndex(radioIdx);
+
+                    // console.log(`voici l'index avec le clic du bouton`, index);
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
